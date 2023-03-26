544bdab3a97c2c455a092f23ddd7372cc0add733
ok-home <ok-home@ya.ru>
2023-03-26 14:07:07 +0700

    change example port

--- example/main/main.c
+++ example/main/main.c
@@ -4,7 +4,7 @@
 #include <freertos/task.h>
 #include <gpio_sw.h>
 
-sw_gpio_cfg_t *cfg21,*cfg22,*cfg23 = NULL;
+sw_gpio_cfg_t *cfg25,*cfg26,*cfg27 = NULL;
 
 //  default event loop handle
 //  gpio - port with key/switch
@@ -21,25 +21,25 @@ void app_main(void)
     // example - 3 different mode key/switch 
 
     // switch with long on/off event on every switch
-    cfg21 = sw_gpio_init(GPIO_NUM_21, SW_DEFAULT_MODE, DEFAULT_BOUNCEDELAY, MIN_DETECT_TIME, sw_gpio_event_handle);
-    if(cfg21 == NULL) return; //error
+    cfg25 = sw_gpio_init(GPIO_NUM_25, SW_DEFAULT_MODE, DEFAULT_BOUNCEDELAY, MIN_DETECT_TIME, sw_gpio_event_handle);
+    if(cfg25 == NULL) return; //error
 
     // switch with short on/off - switch faster then 0.5sek - one event after 0.5sek with counted switch
-    cfg22 = sw_gpio_init(GPIO_NUM_22, SW_DEFAULT_MODE, DEFAULT_BOUNCEDELAY, DEFAULT_DETECT_TIME, sw_gpio_event_handle);
-    if(cfg22 == NULL) return; //error
+    cfg26 = sw_gpio_init(GPIO_NUM_26, SW_DEFAULT_MODE, DEFAULT_BOUNCEDELAY, DEFAULT_DETECT_TIME, sw_gpio_event_handle);
+    if(cfg26 == NULL) return; //error
 
     // key short press/release  < 0.5sek - one event, >0.5sek multiply event
-    cfg23 = sw_gpio_init(GPIO_NUM_23, SW_AUTO_GENERATE_MODE, DEFAULT_BOUNCEDELAY, DEFAULT_DETECT_TIME, sw_gpio_event_handle);
-    if(cfg23 == NULL) return; //error
+    cfg27 = sw_gpio_init(GPIO_NUM_27, SW_AUTO_GENERATE_MODE, DEFAULT_BOUNCEDELAY, DEFAULT_DETECT_TIME, sw_gpio_event_handle);
+    if(cfg27 == NULL) return; //error
     
     for (;;)
     {
         vTaskDelay(100);
     }
     // on exit delete sw
-    sw_gpio_delete(cfg21);
-    sw_gpio_delete(cfg22);
-    sw_gpio_delete(cfg23);
+    sw_gpio_delete(cfg25);
+    sw_gpio_delete(cfg26);
+    sw_gpio_delete(cfg27);
     // event loop delete after delete all sw
     sw_gpio_delete_event_loop();
 }
